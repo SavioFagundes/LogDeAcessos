cb172e016c4592ddf852f0af6b2349a450ddf43f
Savio Fagundes <akirasfm@gmail.com>
2024-08-27 20:02:07 -0300

    Adicionar arquivos de projeto.

---
+++ Conjuntos/Entities/LogRecords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conjuntos.Entities
+{
+    internal class LogRecords
+    {
+        public string UserName { get; set; }
+        public  DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is LogRecords)) return false;
+            LogRecords other = obj as LogRecords;
+            return UserName.Equals(other.UserName);
+
+        }
+    }
+}
---
+++ Conjuntos/Program.cs
@@ -0,0 +1,36 @@
+﻿using Conjuntos.Entities;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Conjuntos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecords> set = new HashSet<LogRecords> ();
+
+            Console.WriteLine("Enter file for name path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecords { UserName =  name, Instant = instant });
+                        Console.WriteLine(line);
+                    }
+                Console.WriteLine("Total Users: " + set.Count);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
